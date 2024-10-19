bd1eb61e9402b31c7415b6a18ff955d0e92b001c
DESKTOP-KTEPBRG\Hardik <smit.s.neoverce@gmail.com>
2024-10-19 15:47:22 +0530

    update : added instagram video downloader

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import { ThemeToggle } from "./theme-toggle";
+import { MobileNav } from "./mobile-nav";
+
+export function Navbar() {
+  return (
+    <header className="h-fit w-full">
+      <nav
+        className={cn(
+          "z-50",
+          "flex h-[3.5rem] px-4",
+          "w-full items-center border-b bg-accent/20"
+        )}
+      >
+        <div className="hidden select-none text-xl font-extrabold uppercase md:block">
+          Downloader
+        </div>
+        <div className="ml-auto flex items-center gap-4">
+          <div className="hidden items-center gap-4 text-lg sm:gap-8 sm:pr-8 md:flex">
+            <Link href="/" className="hover:underline">
+              Home
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/mobile-nav.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+
+import Link from "next/link";
+
+import { usePathname } from "next/navigation";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+import { Button } from "@/components/ui/button";
+
+import { Menu } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { externalLinks, siteLinks } from "@/lib/constants";
+import { Separator } from "./ui/separator";
+
+export function MobileNav({ className }: { className?: string }) {
+  const [open, setOpen] = React.useState(false);
+
+  const pathname = usePathname();
+
+  const handleCloseSheet = () => setOpen(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="outline" className={className}>
+          <Menu className="h-4 w-4" />
+        </Button>
+      </SheetTrigger>
+    </Sheet>
+  );
+}
